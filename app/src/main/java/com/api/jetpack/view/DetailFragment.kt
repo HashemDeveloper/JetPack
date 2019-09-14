d4bd03a5fea3067d6efa40a6a6f6be4de93fe2c1
Abul Hashem Jr <abul@limosys.com>
2019-09-14 10:21:48 -0400

    Change background color dynamically using Palette

--- app/src/main/java/com/api/jetpack/view/DetailFragment.kt
+++ app/src/main/java/com/api/jetpack/view/DetailFragment.kt
@@ -1,7 +1,11 @@
 package com.api.jetpack.view
 
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Base64
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +13,22 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.palette.graphics.Palette
 
 import com.api.jetpack.R
 import com.api.jetpack.databinding.FragmentDetailBinding
 import com.api.jetpack.di.Injectable
 import com.api.jetpack.di.viewmodel.ViewModelFactory
+import com.api.jetpack.model.DogPalette
 import com.api.jetpack.viewmodel.DogDetailViewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.Request
+import com.bumptech.glide.request.target.BitmapImageViewTarget
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.SizeReadyCallback
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -26,6 +40,7 @@ class DetailFragment : Fragment(), Injectable {
     }
     private var dogUUID = 0
     private lateinit var bindingView: FragmentDetailBinding
+    private var imageUrl: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,7 +63,29 @@ class DetailFragment : Fragment(), Injectable {
         this.dogDetailViewModel.getDogDetailLiveData().observe(this, Observer {dogBreed ->
             dogBreed.let {
                 this.bindingView.dog = dogBreed
+                this.imageUrl = it.imageUrl
+                it.imageUrl?.let {
+                    setupBackgroundColor(it)
+                }
             }
         })
     }
+
+    private fun setupBackgroundColor(url: String){
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: SimpleTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val color = palette?.darkVibrantSwatch?.rgb ?: 0
+                            val backgroundPalette = DogPalette(color)
+                            bindingView.palette = backgroundPalette
+                        }
+                }
+
+            })
+    }
 }
+
